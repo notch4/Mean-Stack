4add6c0e73370987969c042214c04c07e64cd709
syco4uraz <yubraj.pokharel555@gmail.com>
2015-03-30 13:59:03 +0545

    Updating the server files

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var express = require('express')
-var app = express()
+var express 			= require('express'),
+	app 				= express(),
+	bodyParser			= require('body-parser'),
+	mongoose			= require('mongoose'),
+	meetupsController 	= require('./server/controllers/meetups-controller');
+
+
+mongoose.connect('mongodb://localhost:27017/mean-demo');
+
+app.use(bodyParser());
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/client/views/index.html')
 })
+
+app.use('/js', express.static(__dirname + '/client/js'));
+
+
+// REST API
+app.get('/api/meetups', meetupsController.list );
+app.post('/api/meetups', meetupsController.create );
 
 var server = app.listen(3000, function () {
   console.log('Example app listening...')
